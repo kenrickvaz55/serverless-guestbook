b56466f5eeb3c6bf9d7ed7e1426ab9c1bd53f97b
kenrickvaz55 <92515215+kenrickvaz55@users.noreply.github.com>
2022-06-28 11:08:07 +0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://930c7d4f.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
